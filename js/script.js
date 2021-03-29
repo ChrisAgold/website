96d0d8298937df5e1f98b93013a1ebf1c0935260
Chris Agold <christophersagold@gmail.com>
2021-03-29 16:15:06 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// JQuery Code //
+$(document).ready(function () {
+    // SuperSlides
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
+
+// JS Code //
+
+// Typed JS
+var typed = new Typed(".typed", {
+    strings: ["Web Developer.", "Full Stack.", "Student."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+});
+
+
+
+
+
+
+
+
+});
