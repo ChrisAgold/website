e33399cd3b4132cce4b67931117b21f7b877a740
Chris Agold <christophersagold@gmail.com>
2021-03-30 10:04:53 -0500

    added owl carousel

--- js/script.js
+++ js/script.js
@@ -10,19 +10,30 @@ $(document).ready(function () {
 // JS Code //
 
 // Typed JS
-var typed = new Typed(".typed", {
-    strings: ["Web Developer.", "Full Stack.", "Always Learning."],
-    typeSpeed: 70,
-    loop: true,
-    startDelay: 1000,
-    showCursor: false
-});
+    var typed = new Typed(".typed", {
+        strings: ["Web Developer.", "Full Stack.", "Always Learning."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
 
-
-
-
-
-
-
+    // Owl-Carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
 
 });
