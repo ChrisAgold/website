f78aa6f9c1e18725caa4f7ec90937cb150b847e6
Chris Agold <christophersagold@gmail.com>
2021-04-04 11:33:23 -0500

    got responsive image features to work

--- js/script.js
+++ js/script.js
@@ -59,6 +59,7 @@ $(document).ready(function () {
             });
         }
 
+
         if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
             $(".counter").each(function() {
                 var element = $(this);
@@ -68,10 +69,15 @@ $(document).ready(function () {
             })
 
             countUpFinished = true;
+
         }
+
+
     });
-    // FancyBox
+
+
     $("[data-fancybox]").fancybox();
+
 
     $(".items").isotope({
         filter: '*',
@@ -80,5 +86,71 @@ $(document).ready(function () {
             easing: 'linear',
             queue: false
         }
-    })
+    });
+
+    $("#filters a").click(function() {
+
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+    });
+
+
+
+    $("#navigation li a").click(function(e) {
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+
+    });
+
+
+
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        var body = $("body");
+
+        if($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        }
+        else {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
+
+
+
+
+    }
+
 });
+
+
+
+
+
+
+
+
+
